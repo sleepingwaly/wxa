d24d67792d103806bd93baf6c55ab4e9fb4109d8
waly <sleepingwaly@aliyun.com>
2017-06-30 11:09:08 +0800

    init

--- src/Prpcrypt.php
+++ src/Prpcrypt.php
@@ -1,53 +1,48 @@
 <?php
-/**
- * Prpcrypt class
- *
- * 
- */
 namespace Wxa;
 
 class Prpcrypt
 {
-	public $key;
+    public $key;
 
-	function Prpcrypt( $k )
-	{
-		$this->key = $k;
-	}
+    function __construct( $k )
+    {
+        $this->key = $k;
+    }
 
-	/**
-	 * 对密文进行解密
-	 * @param string $aesCipher 需要解密的密文
+    /**
+     * 对密文进行解密
+     * @param string $aesCipher 需要解密的密文
      * @param string $aesIV 解密的初始向量
-	 * @return string 解密得到的明文
-	 */
-	public function decrypt( $aesCipher, $aesIV )
-	{
+     * @return string 解密得到的明文
+     */
+    public function decrypt( $aesCipher, $aesIV )
+    {
 
-		try {
-			
-			$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-			
-			mcrypt_generic_init($module, $this->key, $aesIV);
+        try {
 
-			//解密
-			$decrypted = mdecrypt_generic($module, $aesCipher);
-			mcrypt_generic_deinit($module);
-			mcrypt_module_close($module);
-		} catch (Exception $e) {
-			return array(ErrorCode::$IllegalBuffer, null);
-		}
+            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+
+            mcrypt_generic_init($module, $this->key, $aesIV);
+
+            //解密
+            $decrypted = mdecrypt_generic($module, $aesCipher);
+            mcrypt_generic_deinit($module);
+            mcrypt_module_close($module);
+        } catch (Exception $e) {
+            return array(ErrorCode::$IllegalBuffer, null);
+        }
 
 
-		try {
-			//去除补位字符
-			$pkc_encoder = new PKCS7Encoder;
-			$result = $pkc_encoder->decode($decrypted);
+        try {
+            //去除补位字符
+            $pkc_encoder = new PKCS7Encoder;
+            $result = $pkc_encoder->decode($decrypted);
 
-		} catch (Exception $e) {
-			//print $e;
-			return array(ErrorCode::$IllegalBuffer, null);
-		}
-		return array(0, $result);
-	}
+        } catch (Exception $e) {
+            //print $e;
+            return array(ErrorCode::$IllegalBuffer, null);
+        }
+        return array(0, $result);
+    }
 }
